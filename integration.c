c0bc63cb06b923e313f918fe16aa581abcd2822d
Materwan <erwan.mercier972@gmail.com>
2026-02-25 23:18:50 +0100

    integration

--- integration.c
+++ integration.c
@@ -1,38 +1,103 @@
-
-
-
-
 #define INTEGRATION_C
 
 #include "integration.h"
 
 bool setQuadFormula(QuadFormula* qf, char* name)
 {
-  return true;
+    strcpy(qf->name, name);
+    qf->wk = malloc(3 * sizeof(float));
+    qf->xk = malloc(3 * sizeof(float));
+    if (strcmp(name, "left") == 0) {
+        qf->n = 0;
+        qf->xk[0] = 0.0;
+        qf->wk[0] = 1.0;
+    } else if (strcmp(name, "right") == 0) {
+        qf->n = 0;
+        qf->xk[0] = 1.0;
+        qf->wk[0] = 1.0;
+    } else if (strcmp(name, "middle") == 0) {
+        qf->n = 0;
+        qf->xk[0] = 0.5;
+        qf->wk[0] = 1.0;
+    } else if (strcmp(name, "trapezes") == 0) {
+        qf->n = 1;
+        qf->xk[0] = 0.0;
+        qf->xk[1] = 1.0;
+        qf->wk[0] = 0.5;
+        qf->wk[1] = 0.5;
+    } else if (strcmp(name, "simpson") == 0) {
+        qf->n = 2;
+        qf->xk[0] = 0.0;
+        qf->xk[1] = 0.5;
+        qf->xk[2] = 1.0;
+        qf->wk[0] = 1.0 / 6.0;
+        qf->wk[1] = 2.0 / 3.0;
+        qf->wk[2] = 1.0 / 6.0;
+    } else if (strcmp(name, "gauss2") == 0) {
+        qf->n = 1;
+        qf->xk[0] = 1.0 / 2.0 - 1.0 / (2.0 * sqrt(3.0));
+        qf->xk[1] = 1.0 / 2.0 + 1.0 / (2.0 * sqrt(3.0));
+        qf->wk[0] = 0.5;
+        qf->wk[1] = 0.5;
+    } else if (strcmp(name, "gauss3") == 0) {
+        qf->n = 2;
+        qf->xk[0] = 0.5 * (1 - sqrt(3.0 / 5.0));
+        qf->xk[1] = 0.5;
+        qf->xk[2] = 0.5 * (1 + sqrt(3.0 / 5.0));
+        qf->wk[0] = 5.0 / 18.0;
+        qf->wk[1] = 4.0 / 9.0;
+        qf->wk[2] = 5.0 / 18.0;
+    } else {
+        return false;
+    }
+    return true;
 }
 
 /* This function is not required ,but it may useful to debug */
 void printQuadFormula(QuadFormula* qf)
 {
-  printf("Quadratic formula: %s\n", qf->name);
-  /* Print everything else that may be useful */
+    printf("Quadratic formula: %s\n", qf->name);
+    printf("n = %d\n", qf->n);
+    printf("xk\twk\n");
+    for (int i = 0; i <= qf->n; i++) {
+        printf("%.3f\t%.3f\n", qf->xk[i], qf->wk[i]);
+    }
+    /* Print everything else that may be useful */
 }
 
 
 /* Approximate the integral of function f from a to b.
-   - f is a pointer to a function pointer
-   - a and b are the integration bounds
-   - Interval [a,b] is split in N subdivisions [ai,bi]
-   - Integral of f on each subdivision [ai,bi] is approximated by the quadrature formula qf.
+    - f is a pointer to a function pointer
+    - a and b are the integration bounds
+    - Interval [a,b] is split in N subdivisions [ai,bi]
+    - Integral of f on each subdivision [ai,bi] is approximated by the quadrature formula qf.
 */
 double integrate(double (*f)(double), double a, double b, int N, QuadFormula* qf)
 {
-  return 0.0;
+    double ai;
+    double bi;
+    double ratio = (b - a) / N;
+    double diff;
+    double res = 0.0;
+    double sum;
+    for (int i = 0; i < N; i++) {
+        sum = 0.0;
+        ai = a + i * ratio;
+        bi = a + (i + 1) * ratio;
+        diff = bi - ai;
+        for (int j = 0; j <= qf->n; j++) {
+            sum += qf->wk[j] * f(ai + qf->xk[j] * diff);
+        }
+        res += diff * sum;
+    }
+    return res;
 }
 
 double integrate_dx(double (*f)(double), double a, double b, double dx, QuadFormula* qf)
 {
-  return 0.0;
+    int N = (int) round( abs(b-a)/dx );
+    if (N == 0) N = 1;
+    return integrate(f, a, b, N, qf);
 }
 
 
--- test_integration.c
+++ test_integration.c
@@ -7,7 +7,53 @@
 
 /* Code here everything you need to test the integration methods and show your numericzal results */
 
+double f(double x) {
+    return x;
+}
+
+double error(double expected_result, double calculated_result) {
+    return fabs(expected_result - calculated_result);
+}
+
 int main()
 {
-  return 0;
+    int intervals = 10;
+    double a = 0.0;
+    double b = 1.0;
+    double expected_result = 0.5;
+    double result;
+    QuadFormula *qf = malloc(sizeof(QuadFormula));
+    printf("Function : x -> x\tInterval : [%.2f, %.2f]\n", a, b);
+    printf("Expected result : %f\n", expected_result);
+    printf("Number of subdivisions : %d\n", intervals);
+
+    setQuadFormula(qf, "left");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    setQuadFormula(qf, "right");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    setQuadFormula(qf, "middle");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    setQuadFormula(qf, "trapezes");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    setQuadFormula(qf, "simpson");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    setQuadFormula(qf, "gauss2");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    setQuadFormula(qf, "gauss3");
+    // printQuadFormula(qf);
+    result = integrate(f, a, b, intervals, qf);
+    printf("Calculated result : %f\t Error : %f\n", result, error(expected_result, result));
+    return 0;
 }
