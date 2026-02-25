e1fe19b3eaaed147e3445f015725c3681306b095
Materwan <erwan.mercier972@gmail.com>
2026-02-25 22:43:38 +0100

    start

---
+++ pfa.c
@@ -0,0 +1,143 @@
+
+
+// #define GRAPHIC
+
+#define PFA_C
+
+#include "pfa.h"
+
+/* Initialize the integration variables.
+   Arguments :
+   - quadrature : name of the quadrature formula that will be used. It can be "left", "right", 
+     "middle", "trapezes", "simpson", "gauss2" or "gauss3".
+   - dt : a positive value, which will be used to decide the number of subdivisions of an 
+          interval [a,b], when computing the integration.
+          The number of subdivisions will be N such that (b-a)/N ~ dt
+*/
+bool init_integration(char* quadrature, double dt)
+{ 
+  return true;
+}
+
+
+
+/* Density of the normal distribution */
+double phi(double x)
+{
+  return 0.398942280401433 * exp( -x*x/2 );
+}
+
+/* Cumulative distribution function of the normal distribution */
+double PHI(double x)
+{
+  return 0.0;
+}
+
+/* =====================================
+   Finance function: price of an option 
+*/
+double optionPrice(Option* option)
+{
+  return 0.0;
+}
+
+
+
+/* ===============================================*/
+/* Insurance functions */
+
+/* Probability density function (PDF) of variable X.
+   X is the reimbursement in case of a claim from the client.
+*/
+double clientPDF_X(InsuredClient* client, double x)
+{
+  return 0.0;
+}
+
+
+/* Cumulative distribution function (CDF) of variable X.
+   X is the reimbursement in case of a claim from the client.
+*/
+double clientCDF_X(InsuredClient* client, double x)
+{
+  return 0.0;
+}
+
+
+/* ==========================================================*/
+/* Distribution of X1+X2 : static intermediate functions     */
+
+/* The static functions localProductPDF and localPDF_X1X2 take only one
+   argument, of type double.
+   They hence can be integrated: function integrate_dx takes as argument a function pointer f, 
+   where f depends only on one argument (double t).
+   The static functions below can be given as argument to integrate_dx.
+
+   That's why we copy other variables of the final functions (client and x) to local static variables, 
+   and define these static functions depending on only one argument (double t).
+   These local functions can hence be arguments of integrate_dx.
+*/
+static InsuredClient* localClient;
+static double localX;
+
+
+/* This function assumes that static variables localClient and localX have been set.
+   It can be an argument of integrate_dx (since it has the good signature)
+*/
+static double localProductPDF(double t)
+{
+  return clientPDF_X(localClient, localX - t) * clientPDF_X(localClient, t);
+}
+
+/* Density of X1+X2
+
+   This function assumes that static variable localClient has been set.
+   It is called by clientPDF_X1X2
+   It can also be an argument of integrate_dx (since it has the good signature)
+*/
+static double localPDF_X1X2(double x)
+{
+  localX = x;
+  return 0.0;
+} 
+
+
+/* ==========================================================*/
+/* Distribution of X1+X2 : the final functions               */
+
+/* Probability density function (PDF) of variable X1+X2.
+   X1 and X2 are the reimbursements of the two claims from the client (assuming there are 
+   two claims).
+*/
+double clientPDF_X1X2(InsuredClient* client, double x)
+{
+  if ( x<=0 ) return 0.0;
+
+  localClient = client;
+  return localPDF_X1X2(x);
+}
+
+
+/* Cumulative distribution function (CDF) of variable X1+X2.
+   X1 and X2 are the reimbursements of the two claims from the client (assuming there are 
+   two claims).
+*/
+double clientCDF_X1X2(InsuredClient* client, double x)
+{
+  localClient = client;
+
+  return 0.0;
+}
+
+
+
+/* Cumulative distribution function (CDF) of variable S.
+   Variable S is the sum of the reimbursements that the insurance company will pay to client.
+*/
+double clientCDF_S(InsuredClient* client, double x)
+{
+  return 0.0;
+}
+
+
+
